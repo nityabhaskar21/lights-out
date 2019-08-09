a0b68ed22de0f438b5aca06fcf5c8437733e0d48
nityabhaskar21 <nityabhaskar21@gmail.com>
2019-08-09 20:40:53 +0530

    Start react app

--- src/Board.js
+++ src/Board.js
@@ -92,7 +92,7 @@ class Board extends Component {
 
 		if (!this.state.hasWon) {
 			table = (
-				<div classNmae="BoardMargin">
+				<div className="BoardMargin">
 					<h1 className="Board-h1">
 						LIGHTS <span className="Board-h1--2nd">OUT</span>
 					</h1>
